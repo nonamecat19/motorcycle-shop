3efa7941bd3009a540d7b875f105a9280f5262b8
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2022-12-07 09:30:39 +0200

    added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,23 @@ module.exports = {
     },
   },
   daisyui: {
-    themes: ["corporate"],
+    themes: [{
+      'corporate': {
+        primary: "#4b6bfb",
+        secondary: "#7b92b2",
+        accent: "#67cba0",
+        neutral: "#ff7100",
+        "neutral-content": "#edf2f7",
+        "base-100": "#ffffff",
+        "base-content": "#181a2a",
+        "--rounded-box": "0.25rem",
+        "--rounded-btn": ".125rem",
+        "--rounded-badge": ".125rem",
+        "--animation-btn": "0",
+        "--animation-input": "0",
+        "--btn-focus-scale": "1",
+      }
+    }],
     darkTheme: "corporate",
   },
   plugins: [require("daisyui")],
