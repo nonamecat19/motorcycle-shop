0256f2d7b5a18872fb0a063011c9a5185db5ae7a
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-01-11 21:04:58 +0200

    shopping cart fix

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -7,6 +7,7 @@ import {setOrder} from "../../redux/slices/orderSlicer";
 import {Cart, CartElement, ContextStoreType, OrderElement} from "../../Types";
 import {MyContext} from "../ContextStore/ContextStore";
 import {MyInputNumber} from "./MyInputNumber/MyInputNumber";
+import {v4 } from 'uuid'
 
 interface ShoppingCartProps {
 
@@ -44,51 +45,60 @@ export const ShoppingCart: FC<ShoppingCartProps> = ({}) => {
         dispatch(setMotorcycles(tempMotorcycles))
         dispatch(setCart([]))
         dispatch(setNotification(notificationValue))
-        // TODO: fix
         // @ts-ignore
         notifyRef.current.checked = true
     }
 
-    // const elements: JSX.Element = cart.map(([idItem, variation, numberItem]: CartElement) => (
-    //     <Fragment key={idItem}>
-    //         <div className="h-32 image-full flex justify-around items-center">
-    //             <div className="w-60">
-    //                 {/*<img className="h-32 m-auto" src={cache["./" + idItem + ".png"]} alt="Shoes"/>*/}
-    //                 <img className="h-32 m-auto" alt="Shoes"/>
-    //             </div>
-    //             <div className="float-left w-32">
-    //                 <h2 className="card-title">{motorcycles[idItem].brand}</h2>
-    //                 <p>{motorcycles[idItem].model}</p>
-    //             </div>
-    //             <div className="align-item-end text-2xl mr-4">
-    //                 {motorcycles[idItem].price}$
-    //             </div>
-    //             <MyInputNumber
-    //                 id={idItem}
-    //                 number={numberItem}
-    //             />
-    //         </div>
-    //     </Fragment>
-    // ));
-
     return (
         <div className='ShoppingCart'>
-            {/*<input type="checkbox" id="my-modal-cart" className="modal-toggle"/>*/}
-            {/*<div className="modal">*/}
-            {/*    <div className="modal-box cart max-w-4xl relative">*/}
-            {/*        <label htmlFor="my-modal-cart" className="btn btn-sm absolute right-2 top-2">✕</label>*/}
-            {/*        <h3 className="text-lg font-bold">Ваша корзина</h3>*/}
-            {/*        <div className="windowCart">{elements}</div>*/}
-            {/*        <hr/>*/}
-            {/*        <h1>Всього:</h1>*/}
-            {/*        <b className="text-3xl">{fullPrice}$</b>*/}
-            {/*        <label*/}
-            {/*            htmlFor="my-modal-cart"*/}
-            {/*            className="btn btn-m absolute right-5 bottom-5 text-1xl"*/}
-            {/*            onClick={buyProducts}*/}
-            {/*        >Замовити товар</label>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <input type="checkbox" id="my-modal-cart" className="modal-toggle"/>
+            <div className="modal">
+                <div className="modal-box cart max-w-4xl relative">
+                    <label htmlFor="my-modal-cart" className="btn btn-sm absolute right-2 top-2">✕</label>
+                    <h3 className="text-lg font-bold">Ваша корзина</h3>
+                    <div className="windowCart">
+                        {
+                            cart.map(([idItem, variation, numberItem]: CartElement) => {
+                                let key = idItem.toString() + variation.toString() + numberItem.toString()
+                                let currentMotorcycle = motorcycles.find((item: any) => item.id === idItem)
+                                if (currentMotorcycle === undefined)
+                                    return <Fragment key={key}></Fragment>
+
+                                let photo = `http://localhost:8888/data/${currentMotorcycle.variation[variation].photo}`
+
+                                return (
+                                    <Fragment key={key}>
+                                        <div className="h-32 image-full flex justify-around items-center">
+                                            <div className="w-60">
+                                                <img className="h-32 m-auto" src={photo} alt="Shoes"/>
+                                            </div>
+                                            <div className="float-left w-32">
+                                                <h2 className="card-title">{currentMotorcycle.brand}</h2>
+                                                <p>{currentMotorcycle.model}</p>
+                                            </div>
+                                            <div className="align-item-end text-2xl mr-4">
+                                                {currentMotorcycle.price}$
+                                            </div>
+                                            <MyInputNumber
+                                                id={idItem}
+                                                number={numberItem}
+                                            />
+                                        </div>
+                                    </Fragment>
+                                )
+                            })
+                        }
+                    </div>
+                    <hr/>
+                    <h1>Всього:</h1>
+                    <b className="text-3xl">{fullPrice}$</b>
+                    <label
+                        htmlFor="my-modal-cart"
+                        className="btn btn-m absolute right-5 bottom-5 text-1xl"
+                        onClick={buyProducts}
+                    >Замовити товар</label>
+                </div>
+            </div>
         </div>
     )
 }
