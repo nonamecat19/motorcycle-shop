5190b2b4c823fb361a6b095f2fe5ec1c19bffd9a
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-01-11 13:30:27 +0200

    scrollbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -70,7 +70,7 @@ const Navbar: FC<ContentProps> = ({}) => {
         <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                    <img src="https://placeimg.com/80/80/people"/>
+                    <img src="https://www.marketforce.com/hubfs/Icons/kf-user-round-orange.svg"/>
                 </div>
             </label>
             <ul tabIndex={0}
